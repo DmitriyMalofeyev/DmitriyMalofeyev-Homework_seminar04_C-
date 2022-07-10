2d9805883dccf274e5ace22dc029a8cbd92edfb7
DmitriyMalofeyev <et5@list.ru>
2022-07-10 13:24:58 +0300

    решение дз по семинару 4

---
+++ Task 25/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());*/
+
+double exponentiate (int A, int B)
+{
+    double result = Math.Pow(A, B);
+    Console.WriteLine(A+" в степени "+B+" равно "+result);
+    return result;
+}
+
+exponentiate(2,3);
+exponentiate(3,5);
+exponentiate(2,4);
+
+
+
---
+++ Task 27/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumNumbers (int A)
+{
+    int result = 0;
+    while (A !=0)
+    {
+        result = result + A % 10;
+        A = A /10;
+    }
+    Console.WriteLine(result);
+    return result;
+}
+
+
+sumNumbers(452);
+sumNumbers(82);
+sumNumbers(9012);
+sumNumbers(555);
+
+
+
